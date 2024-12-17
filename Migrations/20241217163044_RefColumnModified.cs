d68709172394934a0f7d50e8fcc2de56b90a9162
Nitin Srivastava <63593087+NitinSrivastava529@users.noreply.github.com>
2024-12-17 23:30:55 +0530

    ok

---
+++ Migrations/20241217163044_RefColumnModified.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MadhurAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefColumnModified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RefId",
+                table: "Members",
+                type: "varchar(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RefId",
+                table: "Members",
+                type: "varchar(10)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldNullable: true);
+        }
+    }
+}
